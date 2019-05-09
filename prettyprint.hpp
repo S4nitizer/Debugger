4bfce96e54d2d4b2f261a322b23670814682b2a9
S4nitizer <48378098+S4nitizer@users.noreply.github.com>
2019-05-09 14:11:51 +0500

    Add files via upload

---
+++ prettyprint.hpp
@@ -0,0 +1,89 @@
+template<typename T>
+int SIZE(T (&t)){
+    return t.size();
+}
+
+template<typename T, size_t N>
+int SIZE(T (&t)[N]){
+    return N;
+}
+
+string to_string(char t){
+    return "'" + string({t}) + "'";
+}
+
+string to_string(bool t){
+    return t ? "true" : "false";
+}
+
+string to_string(const string &t, int x1=0, int x2=1e9){
+    string ret = "";
+    for(int i = min(x1,SIZE(t)), _i = min(x2,SIZE(t)-1); i <= _i; ++i){
+        ret += t[i];
+    }
+    return '"' + ret + '"';
+}
+
+string to_string(const char* t){
+    string ret(t);
+    return to_string(ret);
+}
+
+template<size_t N>
+string to_string(const bitset<N> &t, int x1=0, int x2=1e9){
+    string ret = "";
+    for(int i = min(x1,SIZE(t)); i <= min(x2,SIZE(t)-1); ++i){
+        ret += t[i] + '0';
+    }
+    return to_string(ret);
+}
+
+template<typename T, typename... Coords>
+string to_string(const T (&t), int x1=0, int x2=1e9, Coords... C);
+
+template<typename T, typename S>
+string to_string(const pair<T, S> &t){
+    return "(" + to_string(t.first) + ", " + to_string(t.second) + ")";
+}
+
+template<typename T, typename... Coords>
+string to_string(const T (&t), int x1, int x2, Coords... C){
+    string ret = "[";
+    x1 = min(x1, SIZE(t));
+    auto e = begin(t);
+    advance(e,x1);
+    for(int i = x1, _i = min(x2,SIZE(t)-1); i <= _i; ++i){
+        ret += to_string(*e, C...) + (i != _i ? ", " : "");
+        e = next(e);
+    }
+    return ret + "]";
+}
+
+#define dbgv(...) cout << to_string(__VA_ARGS__) << endl;
+
+#define dbg(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgv(__VA_ARGS__);
+
+void dbgr(){;}
+template<typename Heads, typename... Tails>
+void dbgr(Heads H, Tails... T){
+    cout << to_string(H) << " | ";
+    dbgr(T...);
+}
+
+#define dbgr(...) dbgr(__VA_ARGS__); cout << endl;
+
+void dbgm(){;}
+template<typename Heads, typename... Tails>
+void dbgm(Heads H, Tails... T){
+    cout << H << " | ";
+    dbgm(T...);
+}
+
+#define dbgm(...) cout << "[" << #__VA_ARGS__ << "]: "; dbgr(__VA_ARGS__);
+
+void dbgs(){;}
+template<typename Heads, typename... Tails>
+void dbgs(Heads H, Tails... T){
+    cout << H << " ";
+    dbgs(T...);
+}
